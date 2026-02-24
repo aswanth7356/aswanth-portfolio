488accedbf92dfd72cfb2a07dfff817528c64aa4
ASWANTH <150226949+aswanth7356@users.noreply.github.com>
2026-02-24 16:16:24 +0530

    updated

--- src/pages/AllServices.jsx
+++ src/pages/AllServices.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 
 
 const AllServices = () => {

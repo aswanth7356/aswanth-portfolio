089d69a45f0c2d0c7409e7adf802ed71c3e6c07c
ASWANTH <150226949+aswanth7356@users.noreply.github.com>
2026-03-24 14:53:53 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import About from './components/About'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
 import LenisScroll from './components/LenisScroll'
-import Preloader from './components/Preloader'
+// import Preloader from './components/Preloader'
 import Blog from './pages/Blog'
 import ContactPage from './pages/ContactPage'
 import AllWork from './pages/AllWork'
@@ -111,7 +111,7 @@ export default function App() {
                     path="/"
                     element={
                         <>
-                            <Preloader />
+                            {/* <Preloader /> */}
                             
                             <main>
                                 <Header />

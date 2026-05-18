345cdd6defb763a75705937e6e20ce794f08a948
ASWANTH <150226949+aswanth7356@users.noreply.github.com>
2026-05-18 13:03:42 +0530

    canonical issue fixed , react-helemet-async(removed async)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Helmet } from 'react-helmet-async'
+import { Helmet } from 'react-helmet'
 import { Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Work from './components/Work'
